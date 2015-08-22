8dd8ade1a6c25fbcbf66d4678c13ee3aae55e422
MarijnvanKrieken <m.vankrieken@Hotmail.com>
2015-08-22 13:44:17 +0200

    fire states auto change and firefighter texture added

--- Pillo_FireFighters/Assets/scripts/WindowController.cs
+++ Pillo_FireFighters/Assets/scripts/WindowController.cs
@@ -4,6 +4,12 @@ using System.Collections;
 public class WindowController : MonoBehaviour {
 
 	public static string COLLIDER_TAG = "Water";
+	public float fireStrength = 0.0f;
+	public bool burning = false;
+
+	private float maxFireStrength = 99.0f;
+	private int maxFireState = 3;
+	private int fireState = 0;
 
 //	// Use this for initialization
 //	void Start () {
@@ -22,6 +28,69 @@ public class WindowController : MonoBehaviour {
 		}
 	}
 
+	void CheckFireState(){
+
+		/*if (Input.GetKey ("space")) {
+			fireStrength ++;
+			print(fireStrength);
+		}
+		if (Input.GetKey ("x")) {
+			fireStrength --;
+			print(fireStrength);
+		}*/
+	
+		if ((maxFireStrength / (maxFireState-1))* (fireState-1) > fireStrength) {
+
+			fireState --;
+
+			ChangeFireState();
+
+			print ("firestate:" + fireState);
+
+		}
+
+		if ((maxFireStrength / (maxFireState-1))*fireState < fireStrength) {
+
+			fireState ++;
+			
+			ChangeFireState();
+
+			print ("firestate:" + fireState);
+		}
+	
+	}
+
+	void ChangeFireState(){
+
+		switch(fireState)
+		{
+		case 3:
+			//big fire
+
+			break;
+		case 2:
+			//med fire
+
+			break;
+		case 1:
+			//small fire
+
+			break;
+		case 0:
+			//no fire
+
+			break;
+		default:
+			print ("no fireState detected");
+			break;
+		}
+	}
+
+	void Update(){
+
+		CheckFireState ();
+
+	}
 //	void OnTriggerExit(Collider col){
 //		if (col.tag.Equals (COLLIDER_TAG)) {
 //			print ("Exit Trigger");
