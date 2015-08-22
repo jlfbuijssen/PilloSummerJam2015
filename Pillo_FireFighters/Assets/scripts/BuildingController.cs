92f6ed196ea9c5ddea795434c97ddb13a911e87a
Jeffrey Buijssen <jlfbuijssen@gmail.com>
2015-08-22 14:28:42 +0200

    Fire Spreading

--- Pillo_FireFighters/Assets/scripts/BuildingController.cs
+++ Pillo_FireFighters/Assets/scripts/BuildingController.cs
@@ -4,73 +4,67 @@ using System.Collections;
 public class BuildingController : MonoBehaviour {
 
 	public bool raining = false;
-	public float spreadTimer = 10;
-	public float elapsedTime;
+	public float spreadTimer = 10.0F;
+//	public float elapsedTime;
 	public GameObject[] window;
+	float spreadDelay;
 
 	// public constants
 	public static bool RANDOM = true;
 	public static float MAX_RUN_TIME;
 //	public static int WINDOW_ROWS = 3;
 //	public static int WINDOW_COLUMNS = 4;
-	public static int INIT_SPREAD_DELAY = 0;
-	public static int SPREAD_TIMER_RATE = 10;
-
+	public static float INIT_SPREAD_DELAY = 0.0F;
+	public static float SPREAD_TIMER_RATE = 10.0F;
+	public static int LIGHT_ATTEMPTS = 50;
+	public static string WINDOW_TAG = "Window";
 
 
 
 	// Use this for initialization
 	void Start () {
-//		window[0][0];
-//		window[1][0];
-//		window[2][0];
-//		window[3][0];
-//		window[0][1];
-//		window[1][1];
-//		window[2][1];
-//		window[3][1];
-//		window[0][1];
-//		window[1][1];
-//		window[2][1];
-//		window[3][1];
-		window = GameObject.FindGameObjectsWithTag ("Window");
-		//Invoke ("startRain()", MAX_RUN_TIME);
-		//InvokeRepeating ("spreadFire()", INIT_SPREAD_DELAY, SPREAD_TIMER_RATE);
 
+		// Init
+		spreadDelay = INIT_SPREAD_DELAY;
+		// 
+		window = GameObject.FindGameObjectsWithTag (WINDOW_TAG);
 
+		for (int i = 0; i<window.Length; i++) {
+			Debug.Log ("Window "+i+ " is called: "+window[i].name);
+		}
+
+		StartCoroutine (spreadRoutine(INIT_SPREAD_DELAY));
+		Invoke ("startRain()", MAX_RUN_TIME);
 	}
 
-	
 	// Update is called once per frame
 	void Update () {
-		if (!raining) {
-			//print ("Not raining!");
-			if (spreadTimer <= 0){
-				//fireSpreader();
-			}
-			//updateTimers ();
+		if (raining) {
+			// Stop the fire from spreading
+			StopAllCoroutines();
 		}
+	}
+
+	IEnumerator spreadRoutine(float delay){
+		yield return new WaitForSeconds(delay);
+
+		spreadFire ();
+		StartCoroutine ("spreadRoutine", spreadDelay);
 	}
 
 	void spreadFire(){
 		int r;
-		r = Random.Range (0, window.Length);
+		int c = LIGHT_ATTEMPTS;
+		do {
+			r = Random.Range (0, window.Length);
+			c--;
+		} while(!window [r].GetComponent<WindowController>.burning && c > 0);
 
-		window[r].SetActive (true);
+		window[r].GetComponent<WindowController>.burning = true;
 	}
 
-
-	void updateTimers(){
-		elapsedTime += Time.deltaTime;
-		spreadTimer -= Time.deltaTime;
-		
-
-		rainCheck ();
+	void startRain(){
+		raining = true;
 	}
-	
-	void rainCheck(){
-		if (elapsedTime >= MAX_RUN_TIME) {
-			raining = true;
-		}
-	}
+
 }
